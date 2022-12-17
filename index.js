d8d1bb64a5875883c79605a155b9bff06a825755
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-17 12:18:43 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const connectDB = async () => {
-  mongoose.connect("mongoDB:://localhost:27017//e-comm");
+  mongoose.connect("mongodb://localhost:27017/e-comm");
   const productSchema = new mongoose.Schema({});
   const product = mongoose.model("products", productSchema);
   const data = await product.find();
